be3047e98c9ce9647562e92f4c1ea191cffbc3ca
Richard Tatum <rtatum@pm.me>
2024-03-25 15:58:22 +0000

    Update styling of scroll to top to match the return to posts button

--- src/lib/ScrollToTop.svelte
+++ src/lib/ScrollToTop.svelte
@@ -1,9 +1,44 @@
 <script lang="ts">
+    import ChevronUpIcon from "./ChevronUpIcon.svelte";
+
     let showScrollButton = false;
     const scrollToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
     window.onscroll = () => (showScrollButton = window.scrollY > 100);
 </script>
 
 {#if showScrollButton}
-    <button on:click={scrollToTop} class="scroll-to-top">↑</button>
+<div >
+    <button on:click={scrollToTop} class="scroll-to-top">
+        Back to latest
+        <ChevronUpIcon size={12}/>
+    </button>
+</div>
+
 {/if}
+
+<style>
+    button {
+        border: 0px;
+        border-radius: 12px;
+        padding: 5px 10px;
+        position: fixed;
+        top: 20px;
+        cursor: pointer;
+        background-color: #e9e9ed;
+        color: #555;
+        border-radius: 12px;
+        margin: 0 auto;
+        left: 0;
+        right: 0;
+    }
+
+    button:hover {
+        background-color: #d0d0d7;
+    }
+
+    @media (max-width: 768px) {
+        button {
+            top: 10px;
+        }
+    }
+</style>

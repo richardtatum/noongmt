25647e6822a699e982b677fdc3ee6e7724c67420
Richard Tatum <rtatum@pm.me>
2024-03-25 15:57:42 +0000

    Add an obvious button to go to all posts

--- src/routes/SinglePost.svelte
+++ src/routes/SinglePost.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import type { PostData } from "../models/postdata.model";
     import Post from "../lib/Post.svelte";
+    import { Link } from "svelte-routing";
+    import ReturnIcon from "../lib/ReturnIcon.svelte";
 
     export let id: string;
 
@@ -31,4 +33,28 @@
             <p>Loading...</p>
         {/if}
     </div>
+
+    <div class="center">
+        <button>
+            <Link to="/" style="text-decoration: none;">
+                All posts
+                <ReturnIcon size={12} />
+            </Link>
+        </button>
+    </div>    
 </main>
+
+<style>
+    .center {
+        text-align: center;
+        margin-top: -2rem;
+        margin-bottom: 2rem;
+    }
+
+    button {
+        text-decoration: none;
+        border: 0px;
+        border-radius: 12px;
+        padding: 5px 10px;
+    }
+</style>
